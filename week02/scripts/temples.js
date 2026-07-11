f54b51eed880d2fb87bf9eb715d9e751f4107d75
ahule donatus <ahuledonatus40@gmail.com>
2026-07-11 21:21:47 +0100

    update

---
+++ week02/scripts/temples.js
@@ -0,0 +1,15 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = 
+`Last Modified: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    }
+    else {
+        menuButton.textContent = "☰";
+    }
+})
