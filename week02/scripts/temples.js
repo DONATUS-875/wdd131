ecfba59d772eebc1cc5e575c4135ac717c5460ba
ahule donatus <ahuledonatus40@gmail.com>
2026-07-16 18:43:26 +0100

    update

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -6,10 +6,5 @@ const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
-    if (navigation.classList.contains("open")) {
-        menuButton.textContent = "✖";
-    }
-    else {
-        menuButton.textContent = "☰";
-    }
+    menuButton.classList.toggle("open");
 })
